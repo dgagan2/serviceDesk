63b662a56a43829a72e6d8d50e70baf4160bd0c2
dgagan2 <andgag@duck.com>
2023-10-27 00:41:34 -0500

    Se crea la API para cargar imagenes

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,9 +1,11 @@
 import { protect } from '../middleware/auth.handler.js'
+import { verifyToken } from '../services/jwt/tokenSignin.js'
 import { categoryServiceRoute } from './categoryServiceRoute.js'
 import { departmentRoute } from './departmentRoute.js'
 import { loginRoute } from './loginRoute.js'
 import { registerRoute } from './registerRoute.js'
 import { ticketRoute } from './ticketRoutes.js'
+import { uploadImageRoute } from './uploadImage.js'
 import { userRoutes } from './userRoutes.js'
 
 export const routerApi = (app) => {
@@ -16,5 +18,15 @@ export const routerApi = (app) => {
   app.use('/department', departmentRoute)
   app.use('/ticket', ticketRoute)
   app.use('/category', categoryServiceRoute)
+  app.use('/uploadImage', uploadImageRoute)
+  app.post('/validateToken', (req, res) => {
+    const { token } = req.body
+    try {
+      verifyToken(token)
+      res.status(200).send('Token valido')
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  })
   app.get('*', (req, res) => { res.status(404) })
 }
