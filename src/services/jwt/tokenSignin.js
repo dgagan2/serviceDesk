6bd9a1a2e2c18e36afb6e95f71bbdb56d64632d2
dgagan2 <andgag@duck.com>
2023-10-27 00:42:07 -0500

    Se crea la API para validar el token

--- src/services/jwt/tokenSignin.js
+++ src/services/jwt/tokenSignin.js
@@ -1,12 +1,13 @@
 import jsonwebtoken from 'jsonwebtoken'
-
+import dotenv from 'dotenv'
+dotenv.config()
 const { SECRET_KEY } = process.env
 
 function signToken (payload) {
   return jsonwebtoken.sign(payload, SECRET_KEY)
 }
 
-async function verifyToken (token) {
+function verifyToken (token) {
   return jsonwebtoken.verify(token, SECRET_KEY)
 }
 
