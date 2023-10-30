8ae19340d91c932f7d9f428b84b39811bca9201e
dgagan2 <andgag@duck.com>
2023-10-30 00:34:02 -0500

    se agrega la ruta para buscar usuarios

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,9 +3,11 @@ import { stateRoute } from './stateRoute.js'
 import { roleRoute } from './roleRoute.js'
 import { getAllUsers } from '../controllers/user/getUsers.js'
 import { deleteUser } from '../controllers/user/deleteUser.js'
+import { searchUser } from '../controllers/user/searchUser.js'
 export const userRoutes = express.Router()
 
-userRoutes.get('/all', getAllUsers)
+userRoutes.get('/', getAllUsers)
+userRoutes.get('/search', searchUser)
 userRoutes.delete('/:id', deleteUser)
 
 userRoutes.use('/state', stateRoute)
