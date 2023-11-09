2d9cdca4864be4fbc47350bef519725ac8187085
dgagan2 <andgag@duck.com>
2023-11-08 19:04:34 -0500

    finalizar ticket

---
+++ src/controllers/ticket/updateTicket.js
@@ -0,0 +1,26 @@
+import prisma from '../../config/prismaInitialize.js'
+import { searchTicketState } from '../ticket-state/getTicketState.js'
+
+const closeTicket = async (req, res) => {
+  const { numberTicket } = req.body
+  if (!numberTicket) res.status(500).json({ message: 'Empty ticket number' })
+  try {
+    const checkState = await searchTicketState('cerrado')
+    const x = await prisma.ticket.update({
+      where: {
+        numberTicket
+      },
+      data: {
+        idStateTicket: checkState[0].id
+      }
+    })
+    if (x) {
+      res.status(200).json({ message: 'Solicitud finalizada' })
+    }
+  } catch (error) {
+    console.log('error', error)
+    res.status(500).json({ message: 'Something went wrong', error })
+  }
+}
+
+export { closeTicket }
