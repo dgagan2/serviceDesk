2a315da7fd2983c88588ea3cf7f9c3c6da1cadf9
dgagan2 <andgag@duck.com>
2024-02-16 17:58:14 -0500

    validacion usuario esta activo

--- src/utils/auth/checkRolesGql.js
+++ src/utils/auth/checkRolesGql.js
@@ -1,6 +1,10 @@
 import boom from '@hapi/boom';
 
 export default function checkRoles (user, ...roles) {
+  console.log('user.state', user.role);
+  if (user.state !== 'active') {
+    throw boom.unauthorized('Your account is not active');
+  }
   if (!roles.includes(user.role)) {
     throw boom.unauthorized('You do not have the necessary permissions');
   }
