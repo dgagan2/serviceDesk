ddfcaccd0baba13b9f06af10d62ddc84c58737bb
dgagan2 <andgag@duck.com>
2024-02-18 21:33:30 -0500

    Validar si usuario esta activo y los permisos

--- src/utils/auth/checkRolesGql.js
+++ src/utils/auth/checkRolesGql.js
@@ -1,11 +1,19 @@
 import boom from '@hapi/boom';
+import StateService from '../../services/userState.services.js';
+import RoleService from '../../services/userRole.services.js';
 
-export default function checkRoles (user, ...roles) {
-  console.log('user.state', user.role);
-  if (user.state !== 'active') {
+const serviceState = new StateService();
+const serviceRole = new RoleService();
+
+export default async function checkRoles (user, ...roles) {
+  const { stateName } = await serviceState.findOneById(user.state);
+
+  if (stateName !== 'active') {
     throw boom.unauthorized('Your account is not active');
   }
-  if (!roles.includes(user.role)) {
+  const { roleName } = await serviceRole.findOneById(user.role);
+
+  if (!roles.includes(roleName)) {
     throw boom.unauthorized('You do not have the necessary permissions');
   }
 }
