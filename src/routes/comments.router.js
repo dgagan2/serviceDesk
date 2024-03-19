6de8982657ccb94495d641aeaa8cb0345a03c476
dgagan2 <andgag@duck.com>
2024-03-19 11:31:56 -0500

    Eliminacion de rutas sin uso

--- src/routes/comments.router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { checkRoles } from '../middleware/auth.handler.js';
-import passport from 'passport';
-
-export const commentsRoute = express.Router();
-
-commentsRoute.get('/', passport.authenticate('jwt', { session: false }), checkRoles('Admin'), (req, res) => {
-  res.send('GET /comments');
-});
